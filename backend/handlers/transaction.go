c6b6ee6354fb56dcba74e03253cffdb3a5f52ed3
jieggii <jieggii@pm.me>
2022-12-03 01:16:57 +0300

    WIP

---
+++ backend/handlers/transaction.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func TransactionCreate(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+}
+
+func TransactionRead(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+}
+
+func TransactionUpdate(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+}
+
+func TransactionDelete(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+}
---
+++ backend/handlers/user.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func UserCreate(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+}
+
+func UserRead(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+}
+
+func UserUpdate(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+}
+
+func UserDelete(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+}
