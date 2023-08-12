f3398ed7fb599477fdad4a874d4cc64c30e7cc81
jieggii <jieggii@protonmail.com>
2023-08-12 14:49:08 +0300

    Add comment, update error detail

--- internal/http_server/handlers/util/bind_functions.go
+++ internal/http_server/handlers/util/bind_functions.go
@@ -8,13 +8,13 @@ import (
 // BindBody is an alias function for gin.Context.ShouldBind to be used inside handlers.
 func BindBody(c *gin.Context, v interface{}) (ok bool) {
 	if err := c.ShouldBind(v); err != nil {
-		AbortWithStatusBadRequest(c, "invalid request body")
+		AbortWithStatusBadRequest(c, "invalid request body params")
 		return false
 	}
 	return true
 }
 
-// BindQuery TODO
+// BindQuery is an alias for gin.Context.ShouldBindQuery to be used inside handlers.
 func BindQuery(c *gin.Context, v interface{}) (ok bool) {
 	if err := c.ShouldBindQuery(v); err != nil {
 		loggers.Error.Printf("error binding query: %v", err)
