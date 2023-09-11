2679fcfc11175d2f3440a8539df15b8179a1d312
jieggii <jieggii@protonmail.com>
2023-09-11 23:33:38 +0300

    Remove unnecessary logging

--- internal/handlers/util/bind_functions.go
+++ internal/handlers/util/bind_functions.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
-	"github.com/groshi-project/groshi/internal/loggers"
 )
 
 func generateErrorDetails(err error) []string {
@@ -24,8 +23,7 @@ func generateErrorDetails(err error) []string {
 // BindBody is an alias function for gin.Context.ShouldBind to be used inside handlers.
 func BindBody(c *gin.Context, v interface{}) (ok bool) {
 	if err := c.ShouldBind(v); err != nil {
-		loggers.Error.Printf("error binding body: %v", err)
-
+		//loggers.Error.Printf("error binding body: %v", err)
 		AbortWithStatusBadRequest(
 			c,
 			"invalid request body params, please refer to the method documentation",
@@ -39,8 +37,7 @@ func BindBody(c *gin.Context, v interface{}) (ok bool) {
 // BindQuery is an alias for gin.Context.ShouldBindQuery to be used inside handlers.
 func BindQuery(c *gin.Context, v interface{}) (ok bool) {
 	if err := c.ShouldBindQuery(v); err != nil {
-		loggers.Error.Printf("error binding query: %v", err)
-
+		//loggers.Error.Printf("error binding query: %v", err)
 		AbortWithStatusBadRequest(
 			c,
 			"invalid query params, please refer to the method documentation",
